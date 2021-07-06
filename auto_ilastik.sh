7c2f3f07c1bf33f9b0400ef351a0b1fdb89b2d4d
Chaytan Inman <chaytan@cs.washington.edu>
2021-07-06 14:08:59 -0700

    added pixel seg training

--- auto_ilastik.sh
+++ auto_ilastik.sh
@@ -27,12 +27,35 @@ done
 IFS=$SAVEIFS
 echo "Done renaming"
 
+# PIXEL SEGMENTATION
+#XXX instead of making output dir underneath imgs dir, could as for
+# root dir and then make output under that and get images by looking
+# recursively for .tif
+
+#NOTE: this path to run ilastik is ONLY VALID ON YOUR COMPUTER FOR ONE SPECIFIC VERSION OF ILASTIK!! -- if this was hosted on a server that could install ilastik and run the model for you, then this would work
 ~/Applications/ilastik-1.3.3post3-OSX.app/Contents/ilastik-release/run_ilastik.sh \
    	--headless \
-	--project=Hello_World.ilp \
-	--table_filename=/$imagesDir/exported_object_features.csv \
-	--export_source="Object Predictions" \
-	--raw_data $noSpacesImages
+	--project="cyst_pixel_seg.ilp" \
+	--output_format=tif \
+	--output_filename_format=/{dataset_dir}/output/{nickname}.tif \
+	--export_source="Probabilities" \
+
+# OBJECT DETECTION
+
+~/Applications/ilastik-1.3.3post3-OSX.app/Contents/ilastik-release/run_ilastik.sh \
+   	--headless \
+	--project="cyst_object_det.ilp" \
+	--output_format=tif \
+	--output_filename_format=/{dataset_dir}/output/{nickname}.tif \
+	--export_source="Probabilities" \
+
+#~/Applications/ilastik-1.3.3post3-OSX.app/Contents/ilastik-release/run_ilastik.sh \
+#   	--headless \
+#	--project=Hello_World.ilp \
+#	--table_filename=/$imagesDir/exported_object_features.csv \
+#	--export_source="Object Predictions" \
+#	--raw_data $noSpacesImages
+
 #Shouldn't need segmentation image since we're doing both at once
 #   --segmentation_image my_unclassified_objects_1.h5/binary_segmentation_volume
 
