70fc21dc694cc3cb80916137b639fc98c544c238
Chaytan Inman <chaytan@cs.washington.edu>
2021-07-20 17:13:10 -0700

    Made consolidate_csvs work with louisa file naming structure -- NEED TO STANDARDIZE THIS CONVENTION

--- consolidate_csvs.py
+++ consolidate_csvs.py
@@ -1,14 +1,20 @@
 import os
 import sys
 import ntpath
+from pathlib import Path
+
 
 #XXX todo: does this work on windows and will I need it to do so if I run 
 # on hyak instead of individual pcs
+#XXX working on making this take variable number of directories containing
+# csvs and combining all from them
 ''' 
 USAGE: python3 consolidate_csvs.py {path to dir containing output csvs}
     Ex:
     python3 consolidate_csvs.py ~/Desktop/lab/freedman/cysts/object_det/output
-    Useful command: pwd | sed 's/ /\\ /g' | pbcopy
+    Useful commands: 
+        pwd | sed 's/ /\\ /g' | pbcopy
+        pbpaste | xargs python3 consolidate_csvs.py
 
 EFFECTS:
 This script takes the many individual output csvs from a given folder and
@@ -45,12 +51,23 @@ def get_csvs(csv_dir):
     # Checks post condition that some csv files were found
     try:
         file1 = csv_files[0]
-        print("First csv file: " + file1)
+        print("First csv file: " + str(file1))
     except:
         raise FileNotFoundError("Could not find any csv files in ", csv_dir)
 
     return csv_files
 
+def get_csvs_recursive(csv_dir):
+    csv_files = [csv for csv in Path(csv_dir).rglob('*.csv')]
+    try:
+        file1 = csv_files[0]
+        print("First csv file: " + str(file1))
+    except:
+        raise FileNotFoundError("Could not find any csv files in ", csv_dir)
+    return csv_files
+
+# This method of getting the day assumes we are operating on a csv_dir that represents
+# where the images are located, not above it
 def get_day(csv_dir):
     parent_dir = os.path.basename(csv_dir)
     print("Parent dir: ", parent_dir)
@@ -70,12 +87,45 @@ def get_day(csv_dir):
 
     return day
 
+#XXX working here to reconcile possible day ebing below root_dir as a folder name
+# This method of getting the day allows for passing in a root_dir above
+# where the actual csv files are located
+
+# USE WHEN PASSING FILES, NOT DIR PATH
+def get_day_recursive(csv_file):
+    # skip the file
+    parent_dir = os.path.split(csv_file)[0]
+    day = get_day_recursive_helper(parent_dir, 0)
+    return day
+
+def get_day_recursive_helper(csv_dir, height):
+    split_path = os.path.split(csv_dir)
+    print("Split path: ", split_path)
+    parent_dir = split_path[1]
+    day = find_number(parent_dir)
+    # Check grandparent if we don't find parent number
+    if day == "" and height < 4:
+
+        print("Using ", parent_dir, " to find which day the images were taken")
+        day = get_day_recursive_helper(split_path[0], height + 1)
+    #Inv: Gets here if height is greater than 4 or we found non "" day
+    if day == "":
+        raise NameError("Neither the parent nor grandparent directory " +\
+                "indicated which day the images were taken.")
+
+    return day
+
+
 # Checks the given path for a number and returns the first one found
 def find_number(path):
     number = ""
     for word in path.split():
         if word.isdigit():
             number = word
+    if number == "":
+        for word in path.split("_"):
+            if word.isdigit():
+                number = word
     return number
 
 
@@ -110,10 +160,46 @@ def consolidate_csvs(csv_files, day):
     print("Finished writing to output_file") 
     fout.close() # closes output file
 
+
+#XXX WORKING HERE
+def consolidate_csvs_recursive(csv_files):
+    output_file = "out.csv"
+    fout=open(output_file,"a")
+
+    header = _write_first_file(fout, csv_files)
+    
+    # Slicing out the first file that we manually wrote
+    other_files = csv_files[2:]
+    # now the rest, no headers:    
+    for csv in other_files:
+        day = get_day_recursive(csv)
+        print("day: ", day)
+        with open(csv, "r+") as f:
+            #supposed_header = f.readline()
+            # skip headers
+            f.readline()
+
+            # each line is a row
+            for line in f:
+                # Only write lines that are not the header
+                if(line != header):
+                    #print("line before \n", line)
+                    # need to append filename to the csv output to the front
+                    # also appends day column
+                    #processed_line = ntpath.basename(csv) + "," + day + "," + line
+                    processed_line = _process_line(csv, day, line)
+                    print("new line \n", processed_line)
+                    fout.write(processed_line)
+            f.close() # not really needed, closes input file
+
+    print("Finished writing to output_file, ", output_file) 
+    fout.close() # closes output file
+
 # Modifies fout by adding the header and appending the filename to the
 # csv list of headers
 # Returns the header from the first file
-def _write_first_file(fout, csv_files, day):
+#def _write_first_file(fout, csv_files, day):
+def _write_first_file(fout, csv_files):
     # first file, gets the headers
     first_file = open(csv_files[0])
     header = first_file.readline()
@@ -122,6 +208,7 @@ def _write_first_file(fout, csv_files, day):
     new_header = "filename,day," + header
     print("new_header \n", new_header)
     fout.write(new_header)
+    day = get_day_recursive(csv_files[0])
     for line in first_file:
         print("line before \n", line)
         # add filename and day to the file
@@ -144,6 +231,9 @@ if __name__ == "__main__":
     print("args: ", str(sys.argv))
     csv_dir = sys.argv[1]
     rename_files(csv_dir)
-    csvs = get_csvs(csv_dir)
-    day = get_day(csv_dir)
-    consolidate_csvs(csvs, day)
+    #csvs = get_csvs(csv_dir)
+    #day = get_day(csv_dir)
+    #consolidate_csvs(csvs, day)
+    csvs = get_csvs_recursive(csv_dir)
+    consolidate_csvs_recursive(csvs)
+
