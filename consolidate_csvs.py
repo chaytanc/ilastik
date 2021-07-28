139a214c981eb3709c368ebd3e1a47f35048a475
Chaytan Inman <chaytan@cs.washington.edu>
2021-07-27 17:51:13 -0700

    Working on trying Ramila's files with multi-indexing, still have bugs w Louisa's, but can figure out if it is due to file naming by testing same methods but w Ramila's -- also working through IntelliJ not gvim now

--- consolidate_csvs.py
+++ consolidate_csvs.py
@@ -4,8 +4,7 @@ import ntpath
 from pathlib import Path
 import re
 
-
-#XXX todo: does this work on windows and will I need it to do so if I run 
+# XXX todo: does this work on windows and will I need it to do so if I run
 # on hyak instead of individual pcs
 ''' 
 USAGE: python3 consolidate_csvs.py {path to dir containing output csvs}
@@ -38,16 +37,19 @@ The output is stored as out_{day}.csv in the directory from which this script wa
 run.
 '''
 
+
 # Removes spaces and puts in underscores for files
 def rename_files(csv_dir):
     for filename in os.listdir(csv_dir):
-        #print(filename)
+        # print(filename)
         new_name = filename.replace(' ', '_')
         old_file_path = os.path.join(csv_dir, filename)
         new_file_path = os.path.join(csv_dir, new_name)
-        os.rename(old_file_path, new_file_path) 
+        os.rename(old_file_path, new_file_path)
 
-# Returns the path to the renamed file
+    # Returns the path to the renamed file
+
+
 def rename_file(file):
     print("Renaming ", file)
     parts = os.path.split(file)
@@ -57,7 +59,7 @@ def rename_file(file):
     print("New name ", new_name)
     old_file_path = os.path.join(csv_dir, filename)
     new_file_path = os.path.join(csv_dir, new_name)
-    os.rename(old_file_path, new_file_path) 
+    os.rename(old_file_path, new_file_path)
     return new_file_path
 
 
@@ -65,15 +67,16 @@ def rename_file(file):
 # Post condition: finds some csv files in the directory passed
 def get_csvs(csv_dir):
     files = os.listdir(csv_dir)
-    csv_files = [ os.path.join(csv_dir, file) for file in files if file.endswith('.csv') ]
+    csv_files = [os.path.join(csv_dir, file) for file in files if file.endswith('.csv')]
     # Checks post condition that some csv files were found
     try:
         file1 = csv_files[0]
         print("First csv file: " + str(file1))
-    except:
+    except FileNotFoundError:
         raise FileNotFoundError("Could not find any csv files in ", csv_dir)
 
     return csv_files
+
 
 # Looks in all directories below csv_dir for csv files and adds them to the
 # csv file list if they aren't an out.csv type file
@@ -81,21 +84,22 @@ def get_csvs_recursive(csv_dir):
     csv_files = []
     # Excludes strings matching out.*\.csv regex pattern
     pattern = re.compile(r'out.*')
-    #csv_files = [csv for csv in Path(csv_dir).rglob('*.csv')]
+    # csv_files = [csv for csv in Path(csv_dir).rglob('*.csv')]
     for csv in Path(csv_dir).rglob('*.csv'):
-        #print("csv stem: ", csv.stem)
+        # print("csv stem: ", csv.stem)
         bad_csv = re.match(pattern, csv.stem)
         if bad_csv:
             print("skipping csv: ", csv.stem)
         else:
-            #print("good csv: ", csv.stem)
+            # print("good csv: ", csv.stem)
             csv_files.append(csv)
     try:
         file1 = csv_files[0]
         print("First csv file: " + str(file1))
-    except:
+    except FileNotFoundError:
         raise FileNotFoundError("Could not find any csv files in " + str(csv_dir))
     return csv_files
+
 
 # This method of getting the day assumes we are operating on a csv_dir that represents
 # where the images are located, not above it
@@ -106,20 +110,22 @@ def get_day(csv_dir):
     # Check grandparent if we don't find parent number
     if day == "":
         split_path = os.path.split(csv_dir)
-        #print("Split path: ", split_path)
+        # print("Split path: ", split_path)
         split_again = os.path.split(split_path[0])
         grandparent_dir = split_again[1]
         print("Using grandparent ", grandparent_dir, " to find which day the images were taken")
-        #XXX put in an input check whether to continue processing or not
+        # XXX put in an input check whether to continue processing or not
         day = find_number(grandparent_dir)
     if day == "":
-        raise NameError("Neither the parent nor grandparent directory " +\
-                "indicated which day the images were taken.")
+        raise NameError("Neither the parent nor grandparent directory " +
+                        "indicated which day the images were taken.")
 
     return day
 
+
 # This method of getting the day allows for passing in a root_dir above
 # where the actual csv files are located
+
 
 # USE WHEN PASSING FILES, NOT DIR PATH
 def get_day_recursive(csv_file):
@@ -128,20 +134,20 @@ def get_day_recursive(csv_file):
     day = get_day_recursive_helper(parent_dir, 0)
     return day
 
+
 def get_day_recursive_helper(csv_dir, height):
     split_path = os.path.split(csv_dir)
-    #print("Split path: ", split_path)
+    # print("Split path: ", split_path)
     parent_dir = split_path[1]
     day = find_number(parent_dir)
     # Check grandparent if we don't find parent number
     if day == "" and height < 4:
-
         print("Using ", parent_dir, " to find which day the images were taken")
         day = get_day_recursive_helper(split_path[0], height + 1)
-    #Inv: Gets here if height is greater than 4 or we found non "" day
+    # Inv: Gets here if height is greater than 4 or we found non "" day
     if day == "":
-        raise NameError("Neither the parent nor grandparent directory " +\
-                "indicated which day the images were taken.")
+        raise NameError("Neither the parent nor grandparent directory " +
+                        "indicated which day the images were taken.")
 
     return day
 
@@ -161,82 +167,83 @@ def find_number(path):
 
 def consolidate_csvs(csv_files, day):
     output_file = "out_" + day + '.csv'
-    fout=open(output_file,"a")
+    fout = open(output_file, "a")
 
     header = _write_first_file(fout, csv_files, day)
-    
+
     # Slicing out the first file that we manually wrote
     other_files = csv_files[2:]
     # now the rest, no headers:    
     for csv in other_files:
         with open(csv, "r+") as f:
-            #supposed_header = f.readline()
+            # supposed_header = f.readline()
             # skip headers
             f.readline()
 
             # each line is a row
             for line in f:
                 # Only write lines that are not the header
-                if(line != header):
-                    #print("line before \n", line)
+                if line != header:
+                    # print("line before \n", line)
                     # need to append filename to the csv output to the front
                     # also appends day column
-                    #processed_line = ntpath.basename(csv) + "," + day + "," + line
+                    # processed_line = ntpath.basename(csv) + "," + day + "," + line
                     processed_line = _process_line(csv, day, line)
                     print("new line \n", processed_line)
                     fout.write(processed_line)
-            f.close() # not really needed, closes input file
+            f.close()  # not really needed, closes input file
 
-    print("Finished writing to output_file") 
-    fout.close() # closes output file
+    print("Finished writing to output_file")
+    fout.close()  # closes output file
 
 
-#XXX WORKING HERE
+# XXX WORKING HERE
 def consolidate_csvs_recursive(csv_files):
     output_file = "out.csv"
-    fout=open(output_file,"a")
+    fout = open(output_file, "a")
 
     header = _write_first_file(fout, csv_files)
-    
+
     # Slicing out the first file that we manually wrote
     other_files = csv_files[2:]
     # now the rest, no headers:    
     for csv in other_files:
         new_csv_path = rename_file(csv)
         day = get_day_recursive(new_csv_path)
-        #print("day: ", day)
+        # print("day: ", day)
         with open(new_csv_path, "r+") as f:
-            #supposed_header = f.readline()
+            # supposed_header = f.readline()
             # skip headers
             f.readline()
 
             # each line is a row
             for line in f:
                 # Only write lines that are not the header
-                if(line != header):
+                if line != header:
                     print("line before \n", line)
                     # need to append filename to the csv output to the front
                     # also appends day column
-                    #processed_line = ntpath.basename(csv) + "," + day + "," + line
+                    # processed_line = ntpath.basename(csv) + "," + day + "," + line
                     processed_line = _process_line(csv, day, line)
                     print("new line \n", processed_line)
                     fout.write(processed_line)
-            f.close() # not really needed, closes input file
+            f.close()  # not really needed, closes input file
 
-    print("Finished writing to output_file, ", output_file) 
-    fout.close() # closes output file
+    print("Finished writing to output_file, ", output_file)
+    fout.close()  # closes output file
+
 
 # Modifies fout by adding the header and appending the filename to the
 # csv list of headers
 # Returns the header from the first file
-#def _write_first_file(fout, csv_files, day):
+# def _write_first_file(fout, csv_files, day):
 def _write_first_file(fout, csv_files):
     # first file, gets the headers
     new_path = rename_file(csv_files[0])
     first_file = open(new_path)
     header = first_file.readline()
     # adding filename and day column to output
-    #new_header = "filename," + header
+    # new_header = "filename," + header
     new_header = "filename,day," + header
     print("new_header \n", new_header)
     fout.write(new_header)
@@ -244,13 +251,14 @@ def _write_first_file(fout, csv_files):
     for line in first_file:
         print("line before \n", line)
         # add filename and day to the file
-        #first_filename = ntpath.basename(csv_files[0])
-        #line_after = first_filename + line
+        # first_filename = ntpath.basename(csv_files[0])
+        # line_after = first_filename + line
         line_after = _process_line(new_path, day, line)
-        assert(line != line_after)
+        assert (line != line_after)
         print("line after \n", line_after)
         fout.write(line_after)
     return header
+
 
 # Adds filename and day to the line as two new front columns
 # Pass in the csv to get the filename from and the line / row of data we're editing
@@ -258,14 +266,14 @@ def _process_line(csv, day, line):
     processed_line = ntpath.basename(csv) + "," + day + "," + line
     return processed_line
 
+
 if __name__ == "__main__":
-    #for i, arg in enumerate(sys.argv):
+    # for i, arg in enumerate(sys.argv):
     print("args: ", str(sys.argv))
     csv_dir = sys.argv[1]
-    #rename_files(csv_dir)
-    #csvs = get_csvs(csv_dir)
-    #day = get_day(csv_dir)
-    #consolidate_csvs(csvs, day)
+    # rename_files(csv_dir)
+    # csvs = get_csvs(csv_dir)
+    # day = get_day(csv_dir)
+    # consolidate_csvs(csvs, day)
     csvs = get_csvs_recursive(csv_dir)
     consolidate_csvs_recursive(csvs)
-
--- format_data.py
+++ format_data.py
@@ -1,201 +1,1 @@
-import pandas as pd
-import sys
-import os
-
-'''
-#XXX todo
-USAGE: python3 format_data.py {input csv}
-
-{input csv} is the consolidated csv with all the data from Ilastik batch 
-    processing object detection. It will typically be named out_{day}.csv
-    as processed by consolidate_csvs.py.
-    
-PRECONDITIONS:
-    Assumes that row entries in the input csv are unique. There should be 
-    no duplicate treatments on the same day.
-
-EFFECTS:
-    Formats the data from all the processed csv files is filtered for 
-    just the entries detecting "cystanoids" and not other labels
-    like background. Then the area and intensity are found for 
-
-OUTPUT:
-    modified_{input csv}.csv is a more readable and organized verison
-    of the input csv.
-
-'''
-
-#col_headers = ["Treatment", "Well", "Day", "Area", "Diameter", "Mean Intensity"]
-#louisa_col_headers = ["Treatment", "Concentration", "ID", "Day", "Area", "Intensity"]
-
-whose_data = "LOUISA"
-#whose_data = "RAMILA"
-
-# old_headers =filename	object_id	timestep	labelimage_oid	User Label	Predicted Class	Probability of bkgrd	Probability of cystanoid	Minimum intensity_0	Minimum intensity_1	Minimum intensity_2	Maximum intensity_0	Maximum intensity_1	Maximum intensity_2	Size in pixels	Radii of the object_0	Radii of the object_1	Variance of Intensity_0	Variance of Intensity_1	Variance of Intensity_2	Mean Intensity_0	Mean Intensity_1	Mean Intensity_2	Diameter	Bounding Box Minimum_0	Bounding Box Minimum_1	Bounding Box Maximum_0	Bounding Box Maximum_1	Center of the object_0	Center of the object_1 
-
-# new old headers same as before but with filename day... instead of filename...
-
-
-def format_data(df):
-    # get cystanoid prediction rows
-    print("out.csv: ", df)
-    cystanoid_rows = df.loc[df["Predicted Class"] == "cystanoid"].reset_index()
-    filename_df = cystanoid_rows.loc[:,"filename"]
-    #print("filename_df : ", filename_df)
-    with_file_cols_df = _parse_filenames(filename_df)
-    #print("with file info df: ", with_file_cols_df)
-    add_relevant_info(with_file_cols_df, cystanoid_rows)
-    #print("improved df: ", improved_df)
-    well_dfs = _get_unique_wells(with_file_cols_df)
-    get_full_csv(well_dfs)
-
-def format_louisa_data(df):
-    # get cystanoid prediction rows
-    print("out.csv: ", df)
-    cystanoid_rows = df.loc[df["Predicted Class"] == "cystanoid"].reset_index()
-    filename_df = cystanoid_rows.loc[:,"filename"]
-    #print("filename_df : ", filename_df)
-    with_file_cols_df = _parse_louisa_filenames(filename_df)
-    add_days_column(with_file_cols_df, cystanoid_rows)
-    #print("with file info df: ", with_file_cols_df)
-    add_relevant_info_multi(with_file_cols_df, cystanoid_rows)
-    output = "./excel_out.xlsx"
-    with_file_cols_df.to_excel(output)
-    print("Success! saved output to ", output)
-
-# Should make a df that has treatment column, wellname column, and day column
-# and merge with other df
-# PRESUMED FILENAME FORMAT:
-# 210623_control_day_7_f2_table.csv
-# date_treatment_"day"_day_well_"table.csv"
-# Headers to add --> ["Treatment", "Well", "Day"]
-def _parse_filenames(filename_df):
-
-    treatments = []
-    wells = []
-    days = []
-
-    for filename in filename_df:
-        items = filename.split("_")
-        if items[2] ==  "DMSO":
-            treatments.append(items[1] + items[2])
-            wells.append(items[5])
-            days.append(items[4])
-        else:
-            treatments.append(items[1])
-            wells.append(items[4])
-            days.append(items[3])
-    new_df = pd.DataFrame(treatments)
-    new_df.columns = ["Treatment"]
-    new_df["Well"] = wells 
-    new_df["Day"] = days
-    return new_df
-
-# PRESUMED FILENAME FORMAT:
-# day 14/ROCK_5_uM_09_table.csv
-# "day" {day number}/{treatment}_{id}_"table.csv"
-# Will consider treatment and id one field just called treatment
-# Headers to add --> ["Treatment"]
-# need to later add the headers  ["Day X Area", "Day X Intensity"]
-# PRECONDITION: filename must at least contain treatment, id, and table.csv.
-# concentration, I suppose, is optional
-def _parse_louisa_filenames(filename_df):
-    treatments = []
-    concentrations = []
-    ids = []
-
-    for filename in filename_df:
-        items = filename.split("_")
-        try:
-            #print(items, "length: ", len(items))
-            if (len(items) < 3):
-                raise NameError("The filename " + filename + \
-                        " does not contain enough information")
-            # if length == 3 then no concentration (ie dmso) add none to concentration
-            elif (len(items) == 3):
-                concentrations.append(None)
-            else:
-                concentrations.append(items[1])
-
-            treatments.append(items[0])
-            # second to last element is the id
-            ids.append(items[-2])
-        except NameError as e:
-            print("Exception: ", e)
-            print("ERROR: Skipping file ", filename, " beceause it does not have "+\
-                "enough information")
-            continue
-    new_df = pd.DataFrame(treatments)
-    new_df.columns = ["Treatment"]
-    new_df["Concentration"] = concentrations
-    new_df["ID"] = ids
-    return new_df
-
-def add_days_column(in_prog_df, initial_df):
-    in_prog_df["Day"] = initial_df["day"]
-
-# MULTI-INDEXED
-
-def add_relevant_info_multi(in_prog_df, initial_df):
-    areas = initial_df.loc[:,'Size in pixels']
-    intensities = initial_df.loc[:,'Mean Intensity_0']
-    in_prog_df.loc[:,'Area'] = pd.Series(areas, index=in_prog_df.index)
-    in_prog_df.loc[:,'Mean Intensity'] = pd.Series(intensities, index=in_prog_df.index)
-    # Outer index (treatment, concentration, id should all have same # of unique occurences)
-    # inner index -> day
-    in_prog_df.set_index(["Treatment", "Concentration", "ID", "Day"], inplace=True)
-    in_prog_df.sort_index(inplace=True)
-    print("df, multi-indexed: ", in_prog_df)
-
-
-# Modifies in_prog_df
-# adds area, diameter, mean intensity info to the given dataframe
-def add_relevant_info(in_prog_df, initial_df):
-    print("initial df: ", initial_df)
-    areas = initial_df.loc[:,'Size in pixels']
-
-    print("Areas: ", areas)
-    diameters = initial_df.loc[:,'Diameter']
-    intensities = initial_df.loc[:,'Mean Intensity_0']
-    # copy the initial_df so we can add some stuff to it
-    in_prog_df.loc[:,'Area'] = pd.Series(areas, index=in_prog_df.index)
-    in_prog_df.loc[:,'Diameter'] = pd.Series(diameters, index=in_prog_df.index)
-    in_prog_df.loc[:,'Mean Intensity'] = pd.Series(intensities, index=in_prog_df.index)
-
-# returns well_df_dict, a dictionary with keys as unique well names and values of a list of 
-# the rows corresponding to those well names
-def _get_unique_wells(improved_df):
-    
-    well_df_dict = {}
-    wells = improved_df.loc[:,"Well"]
-    for well in wells:
-        row = improved_df.loc[improved_df['Well'] == well]
-        row["Day"] = pd.to_numeric(row["Day"])
-        row = row.sort_values("Day")
-        well_df_dict[well] = row
-    return well_df_dict
-
-# writes full csv to out.csv, merging all the data from each well into one df
-def get_full_csv(well_df_dict):
-    # Concatenates all the individual dataframes representing each well's data
-    big_df = pd.concat(list(well_df_dict.values()))
-    print("final df: ", big_df)
-    # write to csv
-    big_df.to_csv("modified_out.csv")
-
-#XXX working here to add fold change column based on (last day - first day) / first day
-def _calculate_fold_change(improved_df):
-    fold_changes = []
-    #  for each unique treatment, conc, id, get days and areas
-    # fold change is latest day - earliest day / latest day
-    # put fold change for all cols of that unique treat, conc, id and multi index
-
-if __name__ == "__main__":
-    print("args: ", str(sys.argv))
-    csv_file = sys.argv[1]
-    df = pd.read_csv(csv_file)
-    if whose_data == "LOUISA":
-        format_louisa_data(df)
-    if whose_data == "RAMILA":
-        format_data(df)
-
+import pandas as pdimport sysimport os'''USAGE: python3 format_data.py {input csv}{input csv} is the consolidated csv with all the data from Ilastik batch     processing object detection. It will typically be named out_{day}.csv    as processed by consolidate_csvs.py.    PRECONDITIONS:    Assumes that row entries in the input csv are unique. There should be     no duplicate treatments on the same day.EFFECTS:    Formats the data from all the processed csv files is filtered for     just the entries detecting "cystanoids" and not other labels    like background. Then the area and intensity are found for OUTPUT:    modified_{input csv}.csv is a more readable and organized verison    of the input csv.'''#col_headers = ["Treatment", "Well", "Day", "Area", "Diameter", "Mean Intensity"]#louisa_col_headers = ["Treatment", "Concentration", "ID", "Day", "Area", "Intensity"]#whose_data = "LOUISA"whose_data = "RAMILA"# old_headers =filename	object_id	timestep	labelimage_oid	User Label	Predicted Class	Probability of bkgrd# Probability of cystanoid	Minimum intensity_0	Minimum intensity_1	Minimum intensity_2	Maximum intensity_0	Maximum# intensity_1	Maximum intensity_2	Size in pixels	Radii of the object_0	Radii of the object_1	Variance of# Intensity_0	Variance of Intensity_1	Variance of Intensity_2	Mean Intensity_0	Mean Intensity_1	Mean# Intensity_2	Diameter	Bounding Box Minimum_0	Bounding Box Minimum_1	Bounding Box Maximum_0	Bounding Box# Maximum_1	Center of the object_0	Center of the object_1# new old headers same as before but with filename day... instead of filename...# Default is Ramila formattingdef format_data(df):    # get cystanoid prediction rows    print("out.csv: ", df)    cystanoid_rows = df.loc[df["Predicted Class"] == "cystanoid"].reset_index()    filename_df = cystanoid_rows.loc[:, "filename"]    #print("filename_df : ", filename_df)    with_file_cols_df = _parse_filenames(filename_df)    #print("with file info df: ", with_file_cols_df)    # add_relevant_info(with_file_cols_df, cystanoid_rows)    add_relevant_info_multi(with_file_cols_df, cystanoid_rows)    #print("improved df: ", improved_df)    well_dfs = _get_unique_wells(with_file_cols_df)    get_full_csv(well_dfs)def format_louisa_data(df):    # get cystanoid prediction rows    print("out.csv: ", df)    cystanoid_rows = df.loc[df["Predicted Class"] == "cystanoid"].reset_index()    filename_df = cystanoid_rows.loc[:, "filename"]    #print("filename_df : ", filename_df)    with_file_cols_df = _parse_louisa_filenames(filename_df)    add_days_column(with_file_cols_df, cystanoid_rows)    #print("with file info df: ", with_file_cols_df)    add_relevant_info_multi(with_file_cols_df, cystanoid_rows)    output = "./excel_out.xlsx"    with_file_cols_df.to_excel(output)    print("Success! saved output to ", output)# Should make a df that has treatment column, wellname column, and day column# and merge with other df# PRESUMED FILENAME FORMAT:# 210623_control_day_7_f2_table.csv# date_treatment_"day"_day_well_"table.csv"# Headers to add --> ["Treatment", "Well", "Day"]def _parse_filenames(filename_df):    treatments = []    wells = []    days = []    for filename in filename_df:        items = filename.split("_")        print("items ", items)        if items[2] == "DMSO":            treatments.append(items[1] + items[2])            wells.append(items[5])            days.append(items[4])        else:            treatments.append(items[1])            wells.append(items[4])            days.append(items[3])    new_df = pd.DataFrame(treatments)    new_df.columns = ["Treatment"]    #XXX to make multi-index compatible, need to make concentration col    new_df["Well"] = wells    new_df["Day"] = days    return new_df# PRESUMED FILENAME FORMAT:# day 14/ROCK_5_uM_09_table.csv# "day" {day number}/{treatment}_{id}_"table.csv"# Will consider treatment and id one field just called treatment# Headers to add --> ["Treatment"]# need to later add the headers  ["Day X Area", "Day X Intensity"]# PRECONDITION: filename must at least contain treatment, id, and table.csv.# concentration, I suppose, is optionaldef _parse_louisa_filenames(filename_df):    treatments = []    concentrations = []    ids = []    for filename in filename_df:        items = filename.split("_")        try:            #print(items, "length: ", len(items))            if len(items) < 3:                raise NameError("The filename " + filename +                        " does not contain enough information")            # if length == 3 then no concentration (ie dmso) add none to concentration            elif len(items) == 3:                concentrations.append(None)            else:                concentrations.append(items[1])            treatments.append(items[0])            # second to last element is the id            ids.append(items[-2])        except NameError as e:            print("Exception: ", e)            print("ERROR: Skipping file ", filename, " beceause it does not have " +                "enough information")            continue    new_df = pd.DataFrame(treatments)    new_df.columns = ["Treatment"]    new_df["Concentration"] = concentrations    new_df["ID"] = ids    return new_dfdef add_days_column(in_prog_df, initial_df):    in_prog_df["Day"] = initial_df["day"]# MULTI-INDEXEDdef add_relevant_info_multi(in_prog_df, initial_df):    areas = initial_df.loc[:, 'Size in pixels']    intensities = initial_df.loc[:, 'Mean Intensity_0']    in_prog_df.loc[:, 'Area'] = pd.Series(areas, index=in_prog_df.index)    in_prog_df.loc[:, 'Mean Intensity'] = pd.Series(intensities, index=in_prog_df.index)    # Outer index (treatment, concentration, id should all have same # of unique occurences)    # inner index -> day    in_prog_df.set_index(["Treatment", "Concentration", "ID", "Day"], inplace=True)    in_prog_df.sort_index(inplace=True)    print("df, multi-indexed: ", in_prog_df)# Modifies in_prog_df# adds area, diameter, mean intensity info to the given dataframedef add_relevant_info(in_prog_df, initial_df):    print("initial df: ", initial_df)    areas = initial_df.loc[:, 'Size in pixels']    print("Areas: ", areas)    diameters = initial_df.loc[:, 'Diameter']    intensities = initial_df.loc[:, 'Mean Intensity_0']    # copy the initial_df so we can add some stuff to it    in_prog_df.loc[:,'Area'] = pd.Series(areas, index=in_prog_df.index)    in_prog_df.loc[:,'Diameter'] = pd.Series(diameters, index=in_prog_df.index)    in_prog_df.loc[:,'Mean Intensity'] = pd.Series(intensities, index=in_prog_df.index)# returns well_df_dict, a dictionary with keys as unique well names and values of a list of# the rows corresponding to those well namesdef _get_unique_wells(improved_df):    well_df_dict = {}    wells = improved_df.loc[:,"Well"]    for well in wells:        row = improved_df.loc[improved_df['Well'] == well]        row["Day"] = pd.to_numeric(row["Day"])        row = row.sort_values("Day")        well_df_dict[well] = row    return well_df_dict# writes full csv to out.csv, merging all the data from each well into one dfdef get_full_csv(well_df_dict):    # Concatenates all the individual dataframes representing each well's data    big_df = pd.concat(list(well_df_dict.values()))    print("final df: ", big_df)    # write to csv    output_file = "modified_out.xlsx"    print("Success! Writing output to ", output_file)    big_df.to_excel(output_file)#XXX working here to add fold change column based on (last day - first day) / first day# Assuming multi_indexed input as in Louisa'sdef _calculate_fold_change(multi_df):    fold_changes = []    #  for each unique treatment, conc, id, get days and areas    # fold change is latest day - earliest day / latest day    # put fold change for all cols of that unique treat, conc, id and multi indexif __name__ == "__main__":    print("args: ", str(sys.argv))    csv_file = sys.argv[1]    df = pd.read_csv(csv_file)    if whose_data == "LOUISA":        format_louisa_data(df)    if whose_data == "RAMILA":        format_data(df)
